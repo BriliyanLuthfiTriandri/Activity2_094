34d7d9d27275031da1a91819e49abee5683d5bc4
BriliyanLuthfiTriandri <briliyanlt@gmail.com>
2023-03-08 12:19:31 +0700

    first commit

---
+++ latihan1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main (){ //begin
+
+//Numeric luas, P, L
+double Luas, P, L;
+//Display "Masukan Panjangnya = "
+cout << "Masukan Panjangnya = ";
+//Accept P
+cin >> P;
+//Display "Masukan Lebarnya = "
+cout << "Masukan Lebarnya = ";
+//Accept L
+cin >> L;
+//Compute Luas = P*L
+Luas = P*L;
+//Display "Luasnya = " + luas
+cout << "Luasnya = " << Luas;
+}//End
